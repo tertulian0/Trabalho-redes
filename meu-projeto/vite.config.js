e1df24668f86cb9773fd08ad2cd93d5a3af9225c
tertulian0 <mateustertuliano27@gmail.com>
2026-07-11 11:33:37 -0300

    teste5

--- meu-projeto/vite.config.js
+++ meu-projeto/vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Use relative paths so the site works on GitHub Pages
-  base: './Trabalho-redes',
+  base: '/',
 })
